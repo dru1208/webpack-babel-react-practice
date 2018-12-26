7ba35e3198df42c66bad86d48174f84f1c837f2f
andrew hsieh <andrewhsieh1208@gmail.com>
2018-12-26 13:07:51 -0600

    component setup and webpack build completed

---
+++ src/js/components/container/FormContainer.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Input from "../presentational/Input.jsx"
+
+export default class FormContainer extends Component {
+	state = {
+		seo_title: ""
+	}
+
+	handleChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+	}
+
+	render() {
+		const { seo_title } = this.state;
+		return(
+      <form id="article-form">
+      	<Input
+      		text="SEO Title"
+      		label="seo_title"
+      		type="text"
+      		id="seo_title"
+      		value={seo_title}
+      		handleChange={this.handleChange}
+      	/>
+      </form>
+		)
+	}
+}
